dfe23f917ec9d9959d6153391c3b4da4c084f22d
Kenneth Porterfield <kjport3@gmail.com>
2021-05-27 09:52:21 -0400

    Added icon and some placeholder text to the searchbar componenet

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
-    <input value="Vue" @change="onInput" />
+  <div class="input">
+    <i class="fa fa-search icon"></i>
+    <input class="input-field" placeholder="Search Videos" @change="onChange" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchBar',
+  name: "SearchBar",
   methods: {
-    onInput: function(event) {
-      this.$emit('termChange', event.target.value);
-    }
-  }
+    onChange: function(event) {
+      this.$emit("termChange", event.target.value);
+    },
+  },
 };
 </script>
 
@@ -22,5 +23,26 @@ input {
 div {
   text-align: center;
   margin: 20px;
+}
+.fa {
+  color: #aaa;
+}
+.input-field {
+  width: 100%;
+  padding: 4px 10px 5px 35px;
+  text-align: left;
+}
+.input i {
+  position: absolute;
+}
+
+.input {
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.icon {
+  padding: 10px;
+  min-width: 40px;
 }
 </style>
